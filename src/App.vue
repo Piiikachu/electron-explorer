472b93d06eadacecfc31f788126a68c5878ad364
Piiikachu <314475440@qq.com>
2019-10-26 17:12:48 +0800

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <el-container style="height:100%" direction="vertical">
+      <el-header style="height:100%" directrion="vertical">
+        <MyHeader />
+      </el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+
+        <el-main>
+          <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+        </el-main>
+      </el-container>
+
+      <el-footer>Footer</el-footer>
+    </el-container>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
-
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "./components/HelloWorld.vue";
+import MyHeader from "./views/MyHeader.vue";
 @Component({
   components: {
-    HelloWorld,
-  },
+    MyHeader,
+    HelloWorld
+  }
 })
 export default class App extends Vue {}
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
---
+++ src/views/MyHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <el-row >test</el-row>
+    <el-row >test</el-row>
+    <el-row >test</el-row>
+  </div>
+</template>
+<style>
+.el-row {
+  line-height: 35px;
+  align-content: center;
+}
+</style>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "../components/HelloWorld.vue";
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class MyHeader extends Vue {}
+</script>
